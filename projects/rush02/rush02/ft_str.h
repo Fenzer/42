92d5c964001ffcf439a5a6a194b226e12fbebed1
Fenelon PINSON <fpinson@e3r5p20.42.fr>
2017-08-20 15:00:16 +0200

    20/08/17 15:00

---
+++ projects/rush02/rush02/ft_str.h
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabreu <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/19 22:08:15 by mabreu            #+#    #+#             */
+/*   Updated: 2017/08/20 10:38:46 by mabreu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int		ft_atoi(char *str)
+{
+	int i;
+	int sign;
+	int res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (str[i] <= ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i + 1] == '-' || str[i + 1] == '+')
+			return (0);
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (res * sign);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ projects/rush02/rush02/rush00.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lboulin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/05 14:51:07 by lboulin           #+#    #+#             */
+/*   Updated: 2017/08/20 09:19:42 by mabreu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_put.h"
+
+void	start(int x, int y);
+void	mid(int x, int y);
+void	end(int x, int y);
+void	rush(int x, int y);
+int		main(void);
+
+void	rush(int x, int y)
+{
+	if (x <= 0 || y <= 0)
+	{
+	}
+	else
+	{
+		start(x, y);
+	}
+}
+
+void	start(int x, int y)
+{
+	int i;
+
+	i = 2;
+	ft_putchar('o');
+	while (i < x)
+	{
+		ft_putchar('-');
+		i++;
+	}
+	if (x > 1)
+		ft_putchar('o');
+	if (y > 2)
+	{
+		ft_putchar('\n');
+		mid(x, y);
+	}
+	if (y == 2)
+	{
+		ft_putchar('\n');
+		end(x, y);
+	}
+	if (y == 1)
+		ft_putchar('\n');
+}
+
+void	mid(int x, int y)
+{
+	int i;
+	int n;
+
+	i = 2;
+	n = 2;
+	while (n < y)
+	{
+		ft_putchar('|');
+		while (i < x && x > 1)
+		{
+			ft_putchar(' ');
+			i++;
+		}
+		i = 2;
+		if (x > 1)
+			ft_putchar('|');
+		ft_putchar('\n');
+		n++;
+	}
+	end(x, y);
+}
+
+void	end(int x, int y)
+{
+	int i;
+
+	i = 2;
+	if (y > 1)
+	{
+		ft_putchar('o');
+	}
+	while (i < x)
+	{
+		ft_putchar('-');
+		i++;
+	}
+	if (x > 1)
+	{
+		ft_putchar('o');
+	}
+	if (y > 1)
+	{
+		ft_putchar('\n');
+	}
+}
